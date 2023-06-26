751a65dd57e65f4f315c73cefb5f033f42610fd4
Dan Tang <46145303+tydantang@users.noreply.github.com>
2023-06-26 16:26:37 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+template_dir = os.path.abspath('templates')
+app.template_folder = template_dir
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    expression = request.form['expression']
+    result = eval(expression)+1
+    return str(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
